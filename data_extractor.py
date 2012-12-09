0192451d97f21d412d295416e1e54b0a2d096734
Michael Mazzola <mazzola70@gmail.com>
2012-12-09 18:10:35 -0500

    Getting location of each listing using google maps API

--- data_extractor.py
+++ data_extractor.py
@@ -4,12 +4,12 @@
 import pymongo
 import urllib2
 import json
-import yelp_api
+#import yelp_api
 from pymongo import MongoClient
 
 three_taps_search_url = "http://3taps.net/search?category=RHFR&regionCode=USA-NYM-MAN&authToken=3231dd4e4ef940efbe719e30d2481c42&annotations={source_map_google:*%20AND%20source_loc:newyork}"
-three_taps_test_url = "http://3taps.net/search?category=RHFR&regionCode=USA-NYM-MAN&authToken=3231dd4e4ef940efbe719e30d2481c42&annotations={source_map_google:*%20AND%20source_loc:newyork}"
-
+three_taps_test_url = "http://3taps.net/search?category=RHFR&regionCode=USA-NYM-MAN&authToken=3231dd4e4ef940efbe719e30d2481c42&annotations={source_map_google:*%20AND%20source_loc:newyork}&rpp=5"
+google_geolocator_url = "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address="
 
 # Extracts data from the 3taps api into an in memory dict
 def extract_3taps_data():
@@ -19,14 +19,15 @@ def extract_3taps_data():
     response_dict = json.loads(response.read())
     raw_apartment_dict = response_dict["results"]
     # Refine the results to get title, number of bedrooms, price, post url, picture and description
-    refined_apartment_dict = {}
+    refined_apartment_dict = []
     
     for raw_apartment in raw_apartment_dict:
         refined_apartment = {}
-        title = raw_apartment['annotations']['heading']
+        title = raw_apartment['heading']
         refined_apartment.update({'title':title})
-        bedrooms = raw_apartment['annotations']['bedrooms']
-        refined_apartment.update({'bedrooms':bedrooms})
+        if 'bedrooms' in raw_apartment['annotations']:
+            bedrooms = raw_apartment['annotations']['bedrooms']
+            refined_apartment.update({'bedrooms':bedrooms})
         price = raw_apartment['price']
         refined_apartment.update({'price':price})
         post_url = raw_apartment['sourceUrl']
@@ -37,7 +38,16 @@ def extract_3taps_data():
             picture = raw_apartment['images'][0]['full']
             refined_apartment.update({'pic':picture})
 
+        # Get location for each listing
+        raw_location = raw_apartment['annotations']['source_map_google'].split('?q=loc%3a')[1]
+        google_response = urllib2.urlopen(google_geolocator_url + raw_location)
+        google_response_dict = json.loads(google_response.read())
+        refined_location = google_response_dict['results'][0]['geometry']['location']
+        refined_apartment.update({'location':refined_location})
+
+        print "Added new apartment: %s\n" % refined_apartment
         refined_apartment_dict.append(refined_apartment)
+
 
 # Takes an apartment dict and a business dictonary and inserts all the
 # entries into a mongodb
@@ -52,9 +62,9 @@ def import_into_db(apartment_dict, businesses_dict, db):
 
 
 def main():
-    connection = MongoClient()
-    db = connection.findr_database
-    #extract_3taps_data()
+    #connection = MongoClient()
+    #db = connection.findr_database
+    extract_3taps_data()
 
 if __name__ == '__main__':
 	main()
