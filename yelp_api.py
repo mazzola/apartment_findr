4c659682f1f5abc1fa6aa67c2c66bebe17420dbc
Megan <meganjbaker@gmail.com>
2012-12-13 00:09:33 -0500

    Modifying structure of the file so you can call the functionality separately

--- yelp_api.py
+++ yelp_api.py
@@ -1,73 +1,1 @@
-"""Command line interface to the Yelp Search API."""
-
-import json
-import oauth2
-import optparse
-import urllib
-import urllib2
-import pymongo
-from pymongo import MongoClient
-
-
-
-# Setup URL params from options
-url_params = {}
-url_params['location'] = "31st and Broadway, NY"
-#url_params['ll'] = options.point
-url_params['radius_filter'] = 1000
-url_params['category_filter'] = "food"
-url_params['sort'] = 2
-
-consumer_key = 'WoH7PFlWIbmnKuvGdMaNhw'
-consumer_secret = 'GNCVbv7iijc2WbWp3JqA8p0VIok'
-token = '_XgyWyzcARzjGI2f61fB1gZCE8P8ZT_o'
-token_secret = 'U4AgG6kBapGMhPoGTqGQRFSjJ4w'
-
-
-def request(host, path, url_params, consumer_key, consumer_secret, token, token_secret):
-  """Returns response for API request."""
-  # Unsigned URL
-  encoded_params = ''
-  if url_params:
-    encoded_params = urllib.urlencode(url_params)
-  url = 'http://%s%s?%s' % (host, path, encoded_params)
-  print 'URL: %s' % (url,)
-
-  # Sign the URL
-  consumer = oauth2.Consumer(consumer_key, consumer_secret)
-  oauth_request = oauth2.Request('GET', url, {})
-  oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),
-                        'oauth_timestamp': oauth2.generate_timestamp(),
-                        'oauth_token': token,
-                        'oauth_consumer_key': consumer_key})
-
-  token = oauth2.Token(token, token_secret)
-  oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
-  signed_url = oauth_request.to_url()
-  print 'Signed URL: %s\n' % (signed_url,)
-
-  # Connect
-  try:
-    conn = urllib2.urlopen(signed_url, None)
-    try:
-      response = json.loads(conn.read())
-    finally:
-      conn.close()
-  except urllib2.HTTPError, error:
-    response = json.loads(error.read())
-
-  return response
-
-response = request('api.yelp.com', '/v2/search', url_params, consumer_key, consumer_secret, token, token_secret)
-print json.dumps(response, sort_keys=True, indent=2)
-print response['businesses'][1]
-
-conn = MongoClient()
-db =  conn.business_database
-businesses = db.businesses
-business_ids = []
-businesses.insert(response['businesses'])
-print "In the database"
-for business in businesses.find():
-	print business['name'], business['rating']
-
+"""Command line interface to the Yelp Search API."""import jsonimport oauth2import optparseimport urllibimport urllib2import pymongofrom pymongo import MongoClient# Setup URL params from optionsurl_params = {}url_params['radius_filter'] = 500url_params['sort'] = 2 # consumer_key = 'WoH7PFlWIbmnKuvGdMaNhw'# consumer_secret = 'GNCVbv7iijc2WbWp3JqA8p0VIok'# token = '_XgyWyzcARzjGI2f61fB1gZCE8P8ZT_o'# token_secret = 'U4AgG6kBapGMhPoGTqGQRFSjJ4w'consumer_key = "PgP7oMp73RpxLvnyPeB8rQ"consumer_secret	= "fWFixrQzy1bMkL3I7YL2FCv-63A"token = "i6uEHZ_Eh3uRdY3C8dnFYz29lsZKDpkW"token_secret ="GueEboQxj-Zbcnpq247znc8GTVQ"categories = ['food', 'shopping', 'active', 'nightlife', 'education', 'restaurants', 'arts', 'beautysvc']def request(host, path, url_params, consumer_key, consumer_secret, token, token_secret):  """Returns response for API request."""  # Unsigned URL  encoded_params = ''  if url_params:    encoded_params = urllib.urlencode(url_params)  url = 'http://%s%s?%s' % (host, path, encoded_params)  print 'URL: %s' % (url,)  # Sign the URL  consumer = oauth2.Consumer(consumer_key, consumer_secret)  oauth_request = oauth2.Request('GET', url, {})  oauth_request.update({'oauth_nonce': oauth2.generate_nonce(),                        'oauth_timestamp': oauth2.generate_timestamp(),                        'oauth_token': token,                        'oauth_consumer_key': consumer_key})  token = oauth2.Token(token, token_secret)  oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)  signed_url = oauth_request.to_url()  # Connect  try:    conn = urllib2.urlopen(signed_url, None)    try:      response = json.loads(conn.read())    finally:      conn.close()  except urllib2.HTTPError, error:    response = json.loads(error.read())  return response	def get_yelp_scores(apt_id):	conn = MongoClient()	db =  conn.businesses	apts = conn.apartments	businesses = db.businesses	response = request('api.yelp.com', '/v2/search', url_params, consumer_key, consumer_secret, token, token_secret)	cat_score = [0. for c in categories]	location = apts.find({'_id':apt_id}, {'location':1, '_id':0}).next()	url_params['ll']=str(location['lat'])+str(location['long'])	for i, category in enumerate(categories):		url_params['category_filter'] = category		response = request('api.yelp.com', '/v2/search', url_params, consumer_key, consumer_secret, token, token_secret)		ratings_tot = 0		for business in response['businesses']:			business['_id'] = business['id'] #This lets MongoDB use Yelp's ID for the internal database ID, makes it easier to deal with duplicates			#businesses.find_and_modify({ "_id":business['_id']}, businesses.find({ "_id":business['_id']}['nearby_apts'].append(apt_id)), upsert = True)			#businesses.find_and_modify( { "_id":business['id']}, { "$push": { 'nearby_apts': "apt_id"} } , upsert="True")			#business['nearby_apts'] = 			businesses.save(business)	# 			# 		new_apts = (businesses.find({ '_id':business['id']}, { 'nearby_apts': 1 })).next()['nearby_apts'] 	# 		new_apts.append(apt_id[i])	# 		businesses.update( { "_id":business['id']}, { "$set": { 'nearby_apts': new_apts} } , True)			ratings_tot += business['rating']					cat_score[i] = 20.*(3.+ratings_tot)/(1.+len(response['businesses']))	
